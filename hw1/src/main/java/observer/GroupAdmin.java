f726ea014c5d829cb56a01583291861f639d1e2d
Naorl98 <118805710+Naorl98@users.noreply.github.com>
2022-12-30 03:44:59 +0200

    Update GroupAdmin.java

--- hw1/src/main/java/observer/GroupAdmin.java
+++ hw1/src/main/java/observer/GroupAdmin.java
@@ -5,21 +5,36 @@ import java.util.List;
 
 public class GroupAdmin implements Sender {
     private UndoableStringBuilder usb;
-    private List<Member> members = new ArrayList<>();
-    public GroupAdmin() {
+    private List<Member> members = new ArrayList<>(); // save all the members that listen to him.
+    public GroupAdmin() { 
         usb =  new UndoableStringBuilder();
     }
-    //methods to register and unregister observers
+    
+    /**
+     * The method add member to the Members list.
+     * @param obj the new member
+     */
     @Override
     public void register(Member obj) {
         this.members.add(obj);
     }
+     /**
+     * The method remove the member from Members list.
+     * @param obj the new member
+     */
+    
     @Override
     public void unregister(Member obj) {
-        obj.update(new UndoableStringBuilder());
+        obj.update(null);// set his string to null.
         this.members.remove(obj);
     }
-    //Inserts the string into this character sequence.
+     /**
+     * The methodInserts the string into this character sequence,
+     * and send update to the members that in the list.
+     * @param offset Point to the start insert location.
+     * @param str The characters that we need to insert.
+     */
+
     @Override
     public void insert(int offset, String obj) {
         this.usb.insert(offset, obj);
@@ -27,7 +42,12 @@ public class GroupAdmin implements Sender {
             member.update(usb);
         }
     }
-    // Appends the specified string to this character sequence.
+     /**
+     * The
+     * and send update to the members that in the list.
+     * @param offset Point to the start insert location.
+     * @param str The characters that we need to insert.
+     */
     @Override
     public void append(String obj) {
         this.usb.append(obj);
@@ -36,6 +56,7 @@ public class GroupAdmin implements Sender {
         }
     }
     // Removes the characters in a substring of this sequence.
+    //send update to the members that in the list.
     @Override
     public void delete(int start, int end) {
         this.usb.delete(start, end);
@@ -46,6 +67,8 @@ public class GroupAdmin implements Sender {
 
     // Erases the last change done to the document, reverting
     // it to an older state.
+    //send update to the members that in the list.
+
     @Override
     public void undo() {
         this.usb.undo();
